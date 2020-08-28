87fd6754a4c3d58bfbf6a2e0c9e4a5e879833c4c
dkadillak <devin.kadillak@gmail.com>
2020-08-27 22:10:55 -0700

    144-149

--- burger/src/components/Layout/Layout.js
+++ burger/src/components/Layout/Layout.js
@@ -3,11 +3,12 @@ import React from "react";
 import Aux from "../hoc/Aux";
 import ToolBar from "../Navigation/ToolBar/ToolBar";
 import classes from "./Layout.css";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const layout = (props) => (
    <Aux>   
        <ToolBar/>
-        <div> SideDrawer, Backdrop</div>
+       <SideDrawer/>
         <main className={classes.Content}>
             {props.children}
         </main>
